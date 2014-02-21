3427c40b1a5e569d3119c4b13132d0836d0180a1
David <dave.gao@gmail.com>
2014-02-20 22:04:14 -0500

    Updated App class to say Infozen

--- src/main/java/com/infozen/App.java
+++ src/main/java/com/infozen/App.java
@@ -1,13 +1,13 @@
 package com.infozen;
 
 /**
- * Hello World!
+ * Hello Infozen!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Infozen!" );
     }
 }
